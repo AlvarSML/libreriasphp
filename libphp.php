dfe8e1a8eb2612c6e40514d0aa91b77ff92aa5f9
IESATAULFO\DAW214 <javijuventus@hotmail.com>
2017-12-11 19:24:53 +0100

    ordenar

--- libphp.php
+++ libphp.php
@@ -3,4 +3,33 @@
     $index = array_search($usuario, $usuarios);
     return ($passwds[$index] == $pass && $usuario != "");
   }
+
+function ordenar($array, $numElementos){
+$indiceMenor;
+$j;
+$aux;
+	
+	for ($i=0;$i<$numElementos;$i++){
+		$indiceMenor = $i;
+		for ($j=$i+1;j<$numElementos;$j++){
+			
+			if ($array[$j]< $array[$indiceMenor]){
+				
+				$indiceMenor =$j;
+				
+			}
+		}
+		if ($i != $indiceMenor){
+			
+			$aux = $array[$i];
+			$array[$i] = $array[$indiceMenor];
+			$array[$indiceMenor] = $aux;
+		}
+		
+	}
+	
+	
+}
+
+
 ?>
