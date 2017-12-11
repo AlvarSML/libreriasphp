74b9f4511095fccc023ba3403e3b09b8c52b5048
alvarsnow <alvarsnow@gmail.com>
2017-12-11 19:49:31 +0100

    cosas

--- libphp.php
+++ libphp.php
@@ -3,4 +3,23 @@
     $index = array_search($usuario, $usuarios);
     return ($passwds[$index] == $pass && $usuario != "");
   }
+
+function addOrdenado($value,$arr) {
+  if (empty($arr)){
+    $arr[0] = $value;
+  } else if (end($arr)<$value){
+    array_push($arr, $value);
+  }else {
+
+    for ($i=0; $i < count($arr); $i++) { 
+      if ($value<$arr[$i]) {
+        $aux = $arr[$i];
+        $arr[$i] = $value;
+        $value = $aux;
+      }
+    }
+  }
+
+  return $arr;
+}
 ?>
